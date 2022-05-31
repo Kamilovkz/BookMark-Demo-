8e400afb39c6d0a0d1f31022fd50a9c45e6aa5ce
kamilovkz <001kamilov@gmail.com>
2022-05-31 14:31:22 +0600

    Initial Commit

---
+++ BookMark-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookMark-UIKit
+//
+//  Created by Nail Kamilov on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
